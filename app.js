b0b854dca4864d62bad9e23fb96184517142d9dc
madshendricks <mhendricks@mcad.edu>
2017-11-06 15:02:35 -0600

    init

---
+++ app.js
@@ -0,0 +1,8 @@
+// <p id="content"></p>
+
+$('#content').typeIt({
+     strings: ["This is a great string.", "And here we have another great string.."],
+     speed: 25,
+     autoStart: false,
+     startDelay: (4000)
+});
